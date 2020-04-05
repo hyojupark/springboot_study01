1d37466877a67a0d8aa0dad2897794c4b176306c
HyoJu <gkwk011@naver.com>
2020-04-05 18:54:38 +0900

    JPA 연동 및 PostsRepository 생성
    JPA 연동
    PostRepository 생

---
+++ src/main/java/com/springboot01/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.springboot01.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
