0a4995c5a2466746568783e1d8d3bebcb08e1382
HyoJu <gkwk011@naver.com>
2020-04-19 16:28:31 +0900

    Posts 등록, 조회 기능 추가

--- src/main/java/com/springboot01/springboot/web/IndexController.java
+++ src/main/java/com/springboot01/springboot/web/IndexController.java
@@ -1,14 +1,25 @@
 package com.springboot01.springboot.web;
 
-import lombok.NoArgsConstructor;
+import com.springboot01.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@NoArgsConstructor
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
