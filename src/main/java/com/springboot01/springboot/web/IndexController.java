39be21b8f30514ad2ee397db557260c0b68a43bb
HyoJu <gkwk011@naver.com>
2020-04-25 18:22:17 +0900

    Login Annotation 생성

--- src/main/java/com/springboot01/springboot/web/IndexController.java
+++ src/main/java/com/springboot01/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot01.springboot.web;
 
+import com.springboot01.springboot.config.auth.LoginUser;
 import com.springboot01.springboot.config.auth.dto.SessionUser;
 import com.springboot01.springboot.service.posts.PostsService;
 import com.springboot01.springboot.web.dto.PostsResponseDto;
@@ -9,19 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
 //            model.addAttribute("userName", user.getName());
             model.addAttribute("user", user);
